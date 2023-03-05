f65b313650e2de667f1e20d5805f0e9fec4c5ed2
ich_kirich <99396626+ich-kirich@users.noreply.github.com>
2023-03-05 13:59:35 +0300

    refactor: simplify ModalComponent.tsx

--- currency-converter/src/components/ModalComponent/ModalComponent.tsx
+++ currency-converter/src/components/ModalComponent/ModalComponent.tsx
@@ -1,26 +1,30 @@
 import { Box } from "@mui/material";
+import classnames from "classnames";
 import { useEffect, useRef } from "react";
 import { IModalComponentProps } from "../../types/types";
 import styles from "./ModalComponent.module.scss";
 
 function ModalComponent(props: IModalComponentProps) {
   const { children, visible, setVisible } = props;
-  const rootClasses = [styles.modal__invisible];
   const box = useRef<HTMLDivElement>(null);
 
-  if (visible) {
-    rootClasses.push(styles.modal__visible);
-  }
-
   useEffect(() => {
-    const onClick = (e: MouseEvent) =>
-      box.current?.contains(e?.target as Node) || setVisible(false);
+    const onClick = (e: MouseEvent) => {
+      if (box.current && !box.current.contains(e.target as Node)) {
+        setVisible(false);
+      }
+    };
     document.addEventListener("click", onClick);
     return () => document.removeEventListener("click", onClick);
   }, []);
 
   return (
-    <Box className={rootClasses.join(" ")}>
+    <Box
+      className={classnames(
+        styles.modal__invisible,
+        visible && styles.modal__visible,
+      )}
+    >
       <Box className={styles.modal__content} ref={box}>
         {children}
       </Box>
